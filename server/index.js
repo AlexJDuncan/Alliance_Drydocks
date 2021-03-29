15548e7975628c1e2f1d588e0b1d586db7d91c52
Alex Duncan <monkeymedic26@gmail.com>
2021-03-28 22:10:52 -0700

    Hello World!

---
+++ server/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+const axios = require('axios');
+
+const PORT = 8000;
+const PUBLIC_DIR = path.resolve(__dirname, '..', 'public');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('dev'));
+app.use(express.static(PUBLIC_DIR));
+
+app.listen(PORT, () => {
+  console.log(`server listening on localhost: ${PORT}`);
+});
