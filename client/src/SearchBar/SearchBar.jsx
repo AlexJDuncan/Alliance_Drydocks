fb4658252c6a5493fc3cdc21631e06d64abd9fa9
Alex Duncan <monkeymedic26@gmail.com>
2021-04-13 14:56:19 -0700

    adjusted size and position of search input & button

--- client/src/SearchBar/SearchBar.jsx
+++ client/src/SearchBar/SearchBar.jsx
@@ -1,12 +1,36 @@
 import React from 'react';
 import styled from 'styled-components';
 
+const SearchPos = styled.div`
+  display: grid;
+  grid-template-columns: 2f 1fr;
+  grid-template-rows: 1fr;
+  margin-top: 20px;
+  margin-right: 20px;
+  justify-content: right;
+`;
+
+const SearchInput = styled.input`
+  grid-column: 2/3;
+  grid-row: 1/2;
+  width: 200px;
+  margin-right: 60px;
+`;
+
+const SearchButton = styled.button`
+  grid-column: 2/3;
+  grid-row: 1/2;
+  margin-left: 10px;
+  width: 60px;
+  justify-self: right;
+`;
+
 const SearchBar = () => {
   return (
-    <div>
-      <input type="search" />
-      <button type="submit">Search</button>
-    </div>
+    <SearchPos>
+      <SearchInput type="search" />
+      <SearchButton type="submit">Search</SearchButton>
+    </SearchPos>
   )
 }
 
