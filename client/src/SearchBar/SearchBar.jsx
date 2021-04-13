83f3e99791f352d183d0c374d37a12efa5afd736
Alex Duncan <monkeymedic26@gmail.com>
2021-04-13 14:17:23 -0700

    moved search bar to it's own component

---
+++ client/src/SearchBar/SearchBar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const SearchBar = () => {
+  return (
+    <div>
+      <input type="search" />
+      <button type="submit">Search</button>
+    </div>
+  )
+}
+
+export default SearchBar;
--- client/src/ship/Ship.jsx
+++ client/src/ship/Ship.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import Ships from './SampleShips';
+import SearchBar from '../SearchBar/SearchBar';
 
 //* * Styleguides * *//
 
@@ -71,69 +72,68 @@ const Ship = (props) => {
   }
   return(
     <div>
-      <input type="search" />
-
-    <ShipPos>
-      <Name>{Ships[0].name} - Tier {Ships[0].tier}</Name>
-      <Img src={Ships[0].photo} alt="" />
-      <ShipDataGrid>
-        <Data1>
-          Type: {Ships[0].type}<br />
-          Hull<br />
-          &nbsp;
-          lvl-50: {Ships[0].hull.lvl50}<br />
-          &nbsp;
-          lvl-60: {Ships[0].hull.lvl60}<br />
-          &nbsp;
-          lvl-65: {Ships[0].hull.lvl65}<br />
-          Hull Modifier: {Ships[0].hullModifier}<br />
-          Shield Modifier: {Ships[0].shieldModifier}<br />
-          Turn Rate: {Ships[0].turnRate}<br />
-          Impulse Modifier: {Ships[0].impulseModifier}<br />
-          Inertia Rating: {Ships[0].inertiaRating}<br />
-          Warp Core: {Ships[0].warpCore}
-        </Data1>
-        <Data2>
-          Bonus Power:<br />
-          {Ships[0].bonusPower.map((power, idx) => (
-            <div key={idx}>&nbsp;{power}</div>
-            ))}
-          Bridge Officers: {Ships[0].bridgeOfficers.map((officer, idx) => (
-            <div key={idx}>&nbsp;{officer}</div>
-            ))}
-        </Data2>
-        <Data3>
-          Weapons:<br />
-          &nbsp;
-          Fore: {Ships[0].weapons.fore}<br />
-          &nbsp;
-          Aft: {Ships[0].weapons.aft}<br />
-          Device Slots: {Ships[0].deviceSlots}<br />
-          Consoles:<br />
-          &nbsp;
-          Tactical: {Ships[0].consoles.tactical}<br />
-          &nbsp;
-          Engineering: {Ships[0].consoles.engineering}<br />
-          &nbsp;
-          Science: {Ships[0].consoles.science}<br />
-          &nbsp;
-          Universal: {Ships[0].consoles.universal}<br />
-        </Data3>
-        <Data4>
-          Abilities: {Ships[0].abilities.map((ability, idx) => (
-            <div key={idx}>&nbsp;{ability}</div>
-            ))}
-          Admirality Stats:<br />
-          &nbsp;
-          Tactical: {Ships[0].admiralityStats.tactical}<br />
-          &nbsp;
-          Engineering: {Ships[0].admiralityStats.engineering}<br />
-          &nbsp;
-          Science: {Ships[0].admiralityStats.science}
-        </Data4>
-      </ShipDataGrid>
-    </ShipPos>
-  </div>
+      <SearchBar />
+      <ShipPos>
+        <Name>{Ships[0].name} - Tier {Ships[0].tier}</Name>
+        <Img src={Ships[0].photo} alt="" />
+        <ShipDataGrid>
+          <Data1>
+            Type: {Ships[0].type}<br />
+            Hull<br />
+            &nbsp;
+            lvl-50: {Ships[0].hull.lvl50}<br />
+            &nbsp;
+            lvl-60: {Ships[0].hull.lvl60}<br />
+            &nbsp;
+            lvl-65: {Ships[0].hull.lvl65}<br />
+            Hull Modifier: {Ships[0].hullModifier}<br />
+            Shield Modifier: {Ships[0].shieldModifier}<br />
+            Turn Rate: {Ships[0].turnRate}<br />
+            Impulse Modifier: {Ships[0].impulseModifier}<br />
+            Inertia Rating: {Ships[0].inertiaRating}<br />
+            Warp Core: {Ships[0].warpCore}
+          </Data1>
+          <Data2>
+            Bonus Power:<br />
+            {Ships[0].bonusPower.map((power, idx) => (
+              <div key={idx}>&nbsp;{power}</div>
+              ))}
+            Bridge Officers: {Ships[0].bridgeOfficers.map((officer, idx) => (
+              <div key={idx}>&nbsp;{officer}</div>
+              ))}
+          </Data2>
+          <Data3>
+            Weapons:<br />
+            &nbsp;
+            Fore: {Ships[0].weapons.fore}<br />
+            &nbsp;
+            Aft: {Ships[0].weapons.aft}<br />
+            Device Slots: {Ships[0].deviceSlots}<br />
+            Consoles:<br />
+            &nbsp;
+            Tactical: {Ships[0].consoles.tactical}<br />
+            &nbsp;
+            Engineering: {Ships[0].consoles.engineering}<br />
+            &nbsp;
+            Science: {Ships[0].consoles.science}<br />
+            &nbsp;
+            Universal: {Ships[0].consoles.universal}<br />
+          </Data3>
+          <Data4>
+            Abilities: {Ships[0].abilities.map((ability, idx) => (
+              <div key={idx}>&nbsp;{ability}</div>
+              ))}
+            Admirality Stats:<br />
+            &nbsp;
+            Tactical: {Ships[0].admiralityStats.tactical}<br />
+            &nbsp;
+            Engineering: {Ships[0].admiralityStats.engineering}<br />
+            &nbsp;
+            Science: {Ships[0].admiralityStats.science}
+          </Data4>
+        </ShipDataGrid>
+      </ShipPos>
+    </div>
   )
 }
 
