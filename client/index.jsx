e11396022d618bdc8788ea11e5b1e76c0660db94
monkeymedic26 <monkeymedic26@gmail.com>
2022-09-20 20:42:49 -0600

    Add React Router to Index.jsx
    Import BrowserRouter in Index.jsx.  Refactor ReactDom.render() to
    ReactDOM.createRoot() and set in the root variable.  use root.render to
    render the application while wrapping <App /> in BrowserRouter.
    
    Adjust index.html from "app" to "root" so getElementByID gets the right
    element.

--- client/index.jsx
+++ client/index.jsx
@@ -1,5 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './src/App';
+import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+const root = ReactDOM.createRoot(document.getElementById("root"))
+
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
